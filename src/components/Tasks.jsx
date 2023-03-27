8f8f6bfecbed1d2a92ba0814d9dbb0cded25210a
ChaconEfrain <efrachaga@gmail.com>
2023-03-27 16:48:38 -0600

    Add isComplete prop and FilterTasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,20 @@
 import useTasks from "@/hooks/useTasks";
 import React from "react";
 import CreateTask from "./CreateTask";
+import FilterTasks from "./FilterTasks";
 import Task from "./Task";
 
 const Tasks = () => {
   const { tasks } = useTasks();
+
   return (
     <article className="w-full border-2 border-gray-600">
       <CreateTask />
       {!!tasks &&
-        tasks.map(({ task, _id }) => <Task key={_id} task={task} id={_id} />)}
+        tasks.map(({ task, completed, _id }) => (
+          <Task key={_id} task={task} id={_id} isComplete={completed} />
+        ))}
+      <FilterTasks />
     </article>
   );
 };
