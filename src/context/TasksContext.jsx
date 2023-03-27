f8f317323899556e424a550c49e24db970980b26
ChaconEfrain <efrachaga@gmail.com>
2023-03-27 16:50:12 -0600

    Add filter functionality

--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -1,41 +1,68 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import createTask from "@/services/createTask";
 import deleteTask from "@/services/deleteTask";
 import getTasks from "@/services/getTasks";
 import updateTask from "@/services/updateTask";
+import { FILTERS } from "@/constants";
 
 export const TasksContext = createContext();
 
 const TasksContextProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
+  const allTasks = useRef(null);
 
-  useEffect(() => {
-    getTasks().then((tasks) => setTasks(tasks));
-  }, []);
+  const filterTasks = (filter = FILTERS.all) => {
+    let newTasks = [];
+    if (filter === FILTERS.completed) {
+      newTasks = allTasks.current.filter(({ completed }) => completed);
+    } else if (filter === FILTERS.notCompleted) {
+      newTasks = allTasks.current.filter(({ completed }) => !completed);
+    } else {
+      newTasks = allTasks.current;
+    }
+
+    setTasks([...newTasks]);
+  };
 
   const handleDeleteTask = async (id) => {
     await deleteTask(id);
-    const newTasks = tasks.filter((task) => task._id !== id);
+    const newTasks = tasks.filter(({ _id }) => _id !== id);
+    allTasks.current = newTasks;
     setTasks(newTasks);
   };
 
   const handleCreateTask = async (task) => {
     const newTask = await createTask(task);
     const newTasks = [...tasks, newTask];
+    allTasks.current = newTasks;
     setTasks(newTasks);
   };
 
-  const handleUpdateTask = async (id, task) => {
-    const updatedTask = await updateTask(id, task);
-    const newTasks = [...tasks];
-    const taskToReplace = tasks.findIndex(({ _id }) => _id === id);
+  const handleUpdateTask = async (id, task, completed = false) => {
+    const updatedTask = await updateTask(id, task, completed);
+    const newTasks = [...allTasks.current];
+    const taskToReplace = allTasks.current.findIndex(({ _id }) => _id === id);
     newTasks.splice(taskToReplace, 1, updatedTask);
+    allTasks.current = newTasks;
     setTasks(newTasks);
   };
+
+  useEffect(() => {
+    getTasks().then((data) => {
+      setTasks(data);
+      allTasks.current = data;
+    });
+  }, []);
 
   return (
     <TasksContext.Provider
-      value={{ tasks, handleCreateTask, handleDeleteTask, handleUpdateTask }}
+      value={{
+        tasks,
+        handleCreateTask,
+        handleDeleteTask,
+        handleUpdateTask,
+        filterTasks,
+      }}
     >
       {children}
     </TasksContext.Provider>
--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -2,9 +2,20 @@ import { useContext } from "react";
 import { TasksContext } from "@/context/TasksContext";
 
 const useTasks = () => {
-  const { tasks, handleCreateTask, handleDeleteTask, handleUpdateTask } =
-    useContext(TasksContext);
-  return { tasks, handleDeleteTask, handleCreateTask, handleUpdateTask };
+  const {
+    tasks,
+    handleCreateTask,
+    handleDeleteTask,
+    handleUpdateTask,
+    filterTasks,
+  } = useContext(TasksContext);
+  return {
+    tasks,
+    handleDeleteTask,
+    handleCreateTask,
+    handleUpdateTask,
+    filterTasks,
+  };
 };
 
 export default useTasks;
