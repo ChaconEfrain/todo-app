88bb40ede17c2106a21322d24030e72ff59e0b8c
ChaconEfrain <efrachaga@gmail.com>
2023-03-27 16:48:09 -0600

    Delete 'checked' state

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,10 +2,9 @@ import useTasks from "@/hooks/useTasks";
 import { useEffect, useId, useRef, useState } from "react";
 import { CloseIcon, EditIcon } from "./Icons";
 
-const Task = ({ task, id }) => {
+const Task = ({ task, isComplete, id }) => {
   const taskId = useId();
   const taskRef = useRef(null);
-  const [checked, setChecked] = useState(false);
   const [disableEdit, setDisableEdit] = useState(true);
   const [taskInput, setTaskInput] = useState(task);
   const { handleDeleteTask, handleUpdateTask } = useTasks();
@@ -16,6 +15,10 @@ const Task = ({ task, id }) => {
     el.style.height = "0px";
     const scrollHeight = el.scrollHeight;
     el.style.height = scrollHeight + "px";
+  };
+
+  const handleCompleteTask = () => {
+    handleUpdateTask(id, task, !isComplete);
   };
 
   const updateTask = () => {
@@ -33,7 +36,6 @@ const Task = ({ task, id }) => {
   useEffect(() => {
     if (!disableEdit) taskRef.current.focus();
     const el = taskRef.current;
-    // el.style.height = "0px";
     const scrollHeight = el.scrollHeight;
     el.style.height = scrollHeight + "px";
   }, [disableEdit]);
@@ -42,21 +44,22 @@ const Task = ({ task, id }) => {
     <div className="flex justify-between text-lg group border-b-2 border-gray-600">
       <div className="flex gap-2 w-full">
         <input
-          onClick={() => setChecked((prev) => !prev)}
+          onClick={handleCompleteTask}
           id={taskId}
           type="radio"
-          checked={checked}
+          checked={isComplete}
           readOnly
         />
         <textarea
           type="text"
           onChange={handleChange}
           onKeyDown={handleKeyDown}
+          onFocus={(e) => console.log(e)}
           ref={taskRef}
           value={taskInput}
           disabled={disableEdit}
           rows={1}
-          className={`${checked ? "line-through" : ""} resize-none w-full`}
+          className={`${isComplete ? "line-through" : ""} resize-none w-full`}
         />
       </div>
       <div className="opacity-0 transition group-hover:opacity-100 flex items-center">
